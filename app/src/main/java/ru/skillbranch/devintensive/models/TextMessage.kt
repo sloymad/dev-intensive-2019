a99b5c4cbe6d395cc2119072f56d3873c1dcf64a
sloymad <2020327Az>
2019-07-02 18:58:49 +0300

    Refactor BaseMessage

--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -4,13 +4,15 @@ import ru.skillbranch.devintensive.extensions.humanizeDiff
 import java.util.*
 
 class TextMessage(
-    id:String,
-    from:User?,
+    id: String,
+    from: User?,
     chat: Chat,
     isIncoming: Boolean = false,
-    date: Date = Date(),
-    var text:String?
-) : BaseMessage (id, from, chat, isIncoming, date){
-    override fun formatMessage() : String = "id:$id ${from?.firstName}" +
-                "${if (isIncoming) "получил" else "отпарвил"} сообщение \"$text\" ${date.humanizeDiff()}"
+    date : Date = Date(),
+    var text: String?
+
+): BaseMessage(id, from, chat, isIncoming, date) {
+
+    override fun formatMessage(): String = "id:$id ${from?.firstName} " +
+            "${if (isIncoming) "получил" else "отправил"} сообщение \"$text\" ${date.humanizeDiff()}"
 }
