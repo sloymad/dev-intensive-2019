e5fb89dfa6f66549ee740adb577036b4c87cd543
sloymad <sloymad@gmail.com>
2019-07-03 11:22:27 +0300

    Changed BaseMessage class method makeMessage refactor

--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -4,12 +4,12 @@ import ru.skillbranch.devintensive.extensions.humanizeDiff
 import java.util.*
 
 class TextMessage(
-    id: String,
-    from: User?,
-    chat: Chat,
-    isIncoming: Boolean = false,
-    date : Date = Date(),
-    var text: String?
+        id: String,
+        from: User?,
+        chat: Chat,
+        isIncoming: Boolean = false,
+        date : Date = Date(),
+        var text: String?
 
 ): BaseMessage(id, from, chat, isIncoming, date) {
 
